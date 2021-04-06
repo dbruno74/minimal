8c255c99ef31758e529e72535e1cf385c0520cd1
Buğra Aydoğar <bugraaydogar@gmail.com>
2021-04-06 16:16:39 +0300

    Added pv utility to show the progress to the user

--- src/minimal_overlay/bundles/image_deploy/99_deploy_uc20.sh
+++ src/minimal_overlay/bundles/image_deploy/99_deploy_uc20.sh
@@ -2,7 +2,7 @@
 
 echo "Deploying Ubuntu Core 20 image to the /dev/sda"
 
-xzcat /root/ubuntu-core-20-amd64.img.xz | dd of=/dev/sda bs=32M; sync
+xzcat /root/ubuntu-core-20-amd64.img.xz | pv | dd of=/dev/sda bs=32M; sync
 
 echo "Ubuntu Core succesfully deployed to the /dev/sda..."
 echo "Please un-plug the bootable USB Disk and Reboot..."

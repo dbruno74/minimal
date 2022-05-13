85a67db6e723f515745c1ad090f5d0beed188bae
Diego <diego.bruno0101@gmail.com>
2022-05-13 12:06:46 +0200

    Changed grub device to hd0 TPM reset enabled

--- src/minimal_overlay/bundles/image_deploy/99_deploy_uc20.sh
+++ src/minimal_overlay/bundles/image_deploy/99_deploy_uc20.sh
@@ -5,7 +5,7 @@ echo "Deploying Ubuntu Core 20 image to the /dev/sda"
 xzcat /root/ubuntu-core-20-amd64.img.xz | pv | dd of=/dev/sda bs=32M; sync
 
 # In case you need to clear TPM configuration, you could enable the following line
-# echo 5 > /sys/class/tpm/tpm0/ppi/request
+echo 5 > /sys/class/tpm/tpm0/ppi/request
 
 echo "Ubuntu Core succesfully deployed to the /dev/sda..."
 echo "Please un-plug the bootable USB Disk and Reboot..."
